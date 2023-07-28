e37ff6d92ea3f78c3c9ff2c4e4f5999155019cde
johncovenant <ufotjohnamos@gmail.com>
2023-07-28 01:39:11 +0100

    navliink completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 
 import { close, logo, menu } from '../assets';
-import { navlinks } from '../constants';
+import { navLinks } from '../constants';
 
 const Navbar = () => {
   return (
    <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="free-x" className="w-[124px] h-[32px]"/>
 
-      <ul>
-        
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav,index) => (
+          <li 
+          key={nav.id}
+          className={`font-poppins fomt-normal cursor-pointer text-[16px] ${index === navLinks.length -1 ? 'mr-0' : 'mr-10' } text-white`}
+          >
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
       </ul>
    </nav>
   )
