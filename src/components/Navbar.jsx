2e46f5201e55359ef9b0c9e55c6ec3d1cedd701c
johncovenant <ufotjohnamos@gmail.com>
2023-07-28 01:19:25 +0100

    logo done working on nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,11 @@ import { navlinks } from '../constants';
 const Navbar = () => {
   return (
    <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="free-x"/>
+      <img src={logo} alt="free-x" className="w-[124px] h-[32px]"/>
+
+      <ul>
+        
+      </ul>
    </nav>
   )
 }
