b599d1bedb074d63fc47e400710c8f534628e5d0
johncovenant <ufotjohnamos@gmail.com>
2023-07-28 02:51:13 +0100

    starting Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,16 @@
+import styles from '../style';
+import {discount, robot} from '../assets';
 
+// not using return statement since i am returning directly
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+const Hero = () => (
+<section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+
+  <div>
+    
+  </div>
+
+</section>
+)
 
 export default Hero
