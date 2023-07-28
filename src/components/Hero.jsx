7aa105a3ba2ae82925e29bdb776ade0b137726b4
johncovenant <ufotjohnamos@gmail.com>
2023-07-28 03:04:21 +0100

    text responsivity for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,12 @@ import {discount, robot} from '../assets';
 const Hero = () => (
 <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
 
-  <div>
-    
+  <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+
+    <div>
+      
+    </div>
+
   </div>
 
 </section>
