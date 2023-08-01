42e310a2cc70594a1d719b889246566dbb8e1dc7
johncovenant <ufotjohnamos@gmail.com>
2023-08-01 23:47:13 +0100

    get started round button

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,15 @@
-
+import styles from '../style';
+import { arrowUp } from '../assets';
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
+    <div className={`${styles.flexcenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer `}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full `}>
+      <div>
+        
+      </div>
+    </div>
+  </div>
   )
 }
 
