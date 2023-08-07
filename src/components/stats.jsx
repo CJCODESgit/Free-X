729882c8cb214ed1aa4c3ec1f1fb894b31487a75
johncovenant <ufotjohnamos@gmail.com>
2023-08-07 17:04:09 +0100

    working on stats section

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,9 +1,16 @@
 import { stats } from '../constants';
+import styles from '../style';
 
-const Stats = () => {
-  return (
-    <div>stats</div>
-  )
-}
+const Stats = () => (
+  <section className={`${styles.flexcenter} flex-row flex-wrap sm:mb-20 mb-6 `}>
+    {stats.map((stat) => (
+      <div key={stat.id} className=''>
+        <h4>{stat.value}</h4>
+        <p>{stat.title}</p>
+      </div>
+    ))}
+  </section>
+)
+    
 
 export default Stats
