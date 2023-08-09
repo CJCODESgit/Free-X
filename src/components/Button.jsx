ef68f193e746f3c68e6f69fc132d4aada5c0f00f
johncovenant <ufotjohnamos@gmail.com>
2023-08-09 12:38:35 +0100

    animated get started button and worked on card

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 const Button = ( { styles } ) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] txt-primary outline-none ${styles} `}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] txt-primary outline-none animate-pulse ${styles} `}>
         Get Started
     </button>
   )
