17b4491192d3dc28ea0966cb29a0f68823d44a02
johncovenant <ufotjohnamos@gmail.com>
2023-08-09 14:04:17 +0100

    button tweak

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 const Button = ( { styles } ) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] txt-primary outline-none animate-pulse ${styles} `}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] txt-primary outline-none animate-pulse rounded-md ${styles} `}>
         Get Started
     </button>
   )
