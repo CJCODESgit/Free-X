a995736a2f054cdbe43a682814ddbab94e6c389f
johncovenant <ufotjohnamos@gmail.com>
2023-08-09 16:24:11 +0100

    saving progress

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -2,7 +2,11 @@ import { apple, bill, google } from '../assets';
 import styles, {layout} from '../style';
 
 const Billing = () => (
-    <div>Billing</div>
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className='w-[100%] h-[100%] relative z-[5]' />
+      </div>
+    </section>
   )
 
 export default Billing
