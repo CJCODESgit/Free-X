56e896f7e6af65e8e6d346db6fde1e1d18264062
johncovenant <ufotjohnamos@gmail.com>
2023-08-09 15:59:53 +0100

    working on billing importing assests

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,8 @@
+import { apple, bill, google } from '../assets';
+import styles, {layout} from '../style';
 
-
-const Billing = () => {
-  return (
+const Billing = () => (
     <div>Billing</div>
   )
-}
 
 export default Billing
