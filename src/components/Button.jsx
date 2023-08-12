d0c6078f7a2c790efc0003b8506e730ff0395df6
johncovenant <ufotjohnamos@gmail.com>
2023-08-13 00:11:50 +0100

    handled text and Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 const Button = ( { styles } ) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] txt-primary outline-none animate-pulse rounded-md ${styles} `}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] txt-primary outline-none animate-pulse rounded-[10px] ${styles} `}>
         Get Started
     </button>
   )
