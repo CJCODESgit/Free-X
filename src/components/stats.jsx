1ba8033753b22691a249e814fd401bf5f80aed75
johncovenant <ufotjohnamos@gmail.com>
2023-08-12 23:30:14 +0100

    changed stats to statisctics

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,7 +1,7 @@
 import { stats } from '../constants';
 import styles from '../style';
 
-const Stats = () => (
+const Statistics = () => (
   <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6 `}>
     {stats.map((stat) => (
       <div key={stat.id} className={`flex-1 flex justify-start items-center flex-row m-3 `}>
@@ -17,4 +17,4 @@ const Stats = () => (
 )
     
 
-export default Stats
+export default Statistics
