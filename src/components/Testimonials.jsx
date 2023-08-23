332917dc2b6811c8f11669c72e684ba30ce52c08
johncovenant <ufotjohnamos@gmail.com>
2023-08-23 14:49:07 +0100

    created the section and styles and adding content

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,7 +3,14 @@ import styles from '../style';
 import FeedbackCard from './FeedbackCard';
 
 const Testimonials = () => (
-    <div>Testimonials</div>
+    <section id='clients' className={`${styles.paddingY} ${styles.flexcenter} flex-col relative`}>
+      {/* TODO */}
+      <div />
+
+      <div className='w-full flex jsutify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
+        <h1 className={styles.heading2}>What people are <br className='sm:block hidden'/>saying about us</h1>
+      </div>
+    </section>
   )
 
 export default Testimonials
