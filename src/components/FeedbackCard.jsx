2d9a2d0a74389d0bc5bccda975e8161b1669bc21
johncovenant <ufotjohnamos@gmail.com>
2023-08-23 16:31:18 +0100

    corrected font color on feedback card

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -12,7 +12,7 @@ const FeedbackCard = ({content, name, title, img}) => (
 
         <div className='flex flex-col ml-4'>
           <h4 className='font-poppins font-semibold text-[20px] leading-[32px] text-white'>{name}</h4>
-          <p className='font-poppins font-normal text-[16px] leading-[24px] text-dimwhite'>{title}</p>
+          <p className='font-poppins font-normal text-[16px] leading-[24px] text-dimWhite'>{title}</p>
         </div>
       </div>
     </div>
