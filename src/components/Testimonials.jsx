3fc20fb87668bacfacf8c2e861e502882a601eea
johncovenant <ufotjohnamos@gmail.com>
2023-08-23 14:41:32 +0100

    working on testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,10 +2,8 @@ import { feedback } from '../constants';
 import styles from '../style';
 import FeedbackCard from './FeedbackCard';
 
-const Testimonials = () => {
-  return (
+const Testimonials = () => (
     <div>Testimonials</div>
   )
-}
 
 export default Testimonials
