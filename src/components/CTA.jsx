76d9d797efb4e48a8681971755d3a1caeb8c9e3b
johncovenant <ufotjohnamos@gmail.com>
2023-08-25 01:51:07 +0100

    done with CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,12 +2,12 @@ import styles from "../style"
 import Button from "./Button"
 
 const CTA = () => (
-    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding}`}>
-      <div>
-        <h2>Let's try our service now!</h2>
-        <p>All you need to easily receive payments for your work and grow on your freelancing journey.</p>
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className="flex-1 flex flex-col">
+        <h2 className={`${styles.heading2}`}>Let's try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>All you need to easily receive payments for your work and grow on your freelancing journey.</p>
       </div>
-      <div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
         <Button/>
       </div>
     </section>
