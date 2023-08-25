a4b13016562564aadc4f5fd8ba13b66b7d48ec93
johncovenant <ufotjohnamos@gmail.com>
2023-08-25 17:58:54 +0100

    mapping footer links together

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,30 @@ import { footerLinks, socialMedia } from '../constants';
 const Footer = () => (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div className='flex-1 flex flex-col justify-start mr-10'>
+          <img 
+            src={logo}
+            alt="free-x"
+            className='w-[266px] h-[72px] object-contain'
+          />
+          <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make freelancing easy, reliable and secure.</p>
+        </div>
 
+        <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
+          {footerLinks.map((footerLink) => (
+            <div key={footerLink.key} className='flex flex-col ss:my-0 my-4 min-w-[150px]'>
+              <h4>
+                {footerLink.title}
+              </h4>
+              <ul>
+                {footerLink.links.map((link) => (
+                  
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+          
       </div>
     </section>
   )
