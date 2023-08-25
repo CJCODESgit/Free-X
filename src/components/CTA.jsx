a9e8980551c34f4c78516ef9f4bca786c45da9d2
johncovenant <ufotjohnamos@gmail.com>
2023-08-25 01:33:13 +0100

    working on CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,17 @@
+import styles from "../style"
+import Button from "./Button"
 
-
-const CTA = () => {
-  return (
-    <div>CTA</div>
+const CTA = () => (
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding}`}>
+      <div>
+        <h2>Let's try our service now!</h2>
+        <p>All you need to easily receive payments for your work and grow on your freelancing journey.</p>
+      </div>
+      <div>
+        <Button/>
+      </div>
+    </section>
   )
-}
+
 
 export default CTA
